b90d0c22f20673298d87c7cdfbea85f9294d288e
Kevin Decherf <kevin@kdecherf.com>
2019-01-06 19:23:01 +0100

    EntriesExport/epub: revert c779373f, move exportinfo to the end of the book
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- src/Wallabag/CoreBundle/Helper/EntriesExport.php
+++ src/Wallabag/CoreBundle/Helper/EntriesExport.php
@@ -189,11 +189,13 @@ class EntriesExport
             }
             $filename = sha1($entry->getTitle());
 
-            $titlepage = $content_start . '<h1>' . $entry->getTitle() . '</h1>' . $this->getExportInformation('PHPePub') . $bookEnd;
+            $titlepage = $content_start . '<h1>' . $entry->getTitle() . '</h1>' . $bookEnd;
             $book->addChapter("Entry {$i} of {$entryCount}", "{$filename}_cover.html", $titlepage, true, EPub::EXTERNAL_REF_ADD);
             $chapter = $content_start . $entry->getContent() . $bookEnd;
             $book->addChapter($entry->getTitle(), "{$filename}.html", $chapter, true, EPub::EXTERNAL_REF_ADD);
         }
+
+        $book->addChapter('Notices', 'Cover2.html', $content_start . $this->getExportInformation('PHPePub') . $bookEnd);
 
         return Response::create(
             $book->getBook(),
